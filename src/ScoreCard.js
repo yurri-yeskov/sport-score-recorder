3d25aac84603fe3613fec3dae8e4c05afe70814f
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-18 11:17:17 -0500

    start framing components

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import GolfRoundForm from './GolfRoundForm'
+import GolfSearchPage from './GolfSearchPage'
+import GolfScoreCard from './GolfScoreCard'
 
 export default function ScoreCard(props) {
     return (
         <div className="ScoreCard">
-            <GolfRoundForm/>
-            <GolfAddCourseForm/>
-            <GolfAddPlayerForm/>
-            <GolfScoreCard/>
+            <GolfRoundForm {...props}/>
+            <GolfSearchPage {...props}/>
+            <GolfScoreCard {...props}/>
         </div>
     )
 }
