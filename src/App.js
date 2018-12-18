d7fb989fd915c444b51aefd246c0d3f540ed43d5
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-18 11:58:30 -0500

    integrate react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import store from './store'
-import ScoreCardConnector from './ ScoreCardConnector'
+import GolfRoundConnector from './GolfRoundConnector'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+
         <div className="App">
-          <ScoreCardConnector />
+          <GolfRoundConnector />
         </div>
-      </Provider>
-    );
+    )
   }
 }
 
