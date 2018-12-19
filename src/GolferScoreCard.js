d03bae13005b7310985d7468fce39defcfe66e6e
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-19 08:44:59 -0500

    swap cols rows on scorecard

--- src/GolferScoreCard.js
+++ src/GolferScoreCard.js
@@ -6,26 +6,32 @@ export default function GolferScoreCard(props) {
     const playerHoleScores = props.holeScores.filter(holeScore => holeScore.course_score_id === playerCourseScore.id)
     return (
         <div className='GolferScoreCard'>
-            <h1>{props.player.name}</h1>
             <div className='playerScores'>
-                <div className='scoreCardHoles'>
+                <h1>{props.player.name}</h1>
+                <div className='scoreCardHeaders'>
                     <h4>Hole #</h4>
-                    {playerHoles.map(hole => <h6 key={hole.id}>{hole.number}</h6>)}
+                    <h4>Par</h4>
+                    <h4>Shots</h4>
+                    <h4>Score</h4>
                     <h6>IN</h6>
                     <h6>OUT</h6>
                     <h6>TOT</h6>
                 </div>
-                <div className='scoreCardPars'>
-                    <h4>Par</h4>
-                    {playerHoles.map(hole => <h6 key={hole.id}>{hole.par}</h6>)}
-                </div>
-                <div className='scoreCardShots'>
-                    <h4>Shots</h4>
-                    {playerHoleScores.map(phs=> <h6 key={phs.id}>{phs.shots}</h6>)}
-                </div>
                 <div className='scoreCardScores'>
-                    <h4>Score</h4>
-                    {playerHoleScores.map(phs => <h6 key={phs.id}>{phs.shots - playerHoles.find(hole => hole.id === phs.hole_id).par}</h6>)}
+                    {playerHoles.map(hole => {
+                        const phs = playerHoleScores.find(holeScore => holeScore.hole_id === hole.id)
+                        const playerScore = phs.shots - playerHoles.find(hole => hole.id === phs.hole_id).par
+                        return (
+                            <div key={phs.id} className='scoreCardHole'>
+                                <h6>{hole.number}</h6>
+                                <h6>{hole.par}</h6>
+                                <h6>{phs.shots}</h6>
+                                <h6>{playerScore}</h6>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='scoreCardTotals'>
                 </div>
             </div>
         </div>
