0aeef18514124888aad2888e78be3b0ba6dd7129
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-19 18:16:18 -0500

    use currentHoleScores for scorecard

--- src/scorecard/GolfScoreCard.js
+++ src/scorecard/GolfScoreCard.js
@@ -3,10 +3,10 @@ import GolferScoreCardHeader from './GolferScoreCardHeader'
 import GolferScoreCardScores from './GolferScoreCardScores'
 
 export default function GolfScoreCard(props) {
-    const {player, holes, currentCourse, currentCourseScores, holeScores} = props
+    const {player, holes, currentCourse, currentCourseScores, currentHoleScores} = props
     const playerHoles = holes.filter(hole => hole.course_id === currentCourse.id)
     const playerCourseScore = currentCourseScores.find(ccs => ccs.player_id === player.id)
-    const playerHoleScores = holeScores.filter(holeScore => holeScore.course_score_id === playerCourseScore.id)
+    const playerHoleScores = currentHoleScores.filter(holeScore => holeScore.course_score_id === playerCourseScore.id)
     return (
         <div className='GolfScoreCard'>
             <GolferScoreCardHeader {...props} playerHoleScores={playerHoleScores}/>
