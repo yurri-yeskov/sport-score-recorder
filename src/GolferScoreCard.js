3699dad49f61d2d5e0e68461ac808d7775830880
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-18 19:56:26 -0500

    frame scorecard

--- src/GolferScoreCard.js
+++ src/GolferScoreCard.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
 export default function GolferScoreCard(props) {
+    const playerHoles = props.holes.filter(hole => hole.course_id === props.currentCourse.id)
+    const playerCourseScore = props.currentCourseScores.find(ccs => ccs.player_id === props.player.id)
+    const playerHoleScores = props.holeScores.filter(holeScore => holeScore.course_score_id === playerCourseScore.id)
+    console.log(playerCourseScore, playerHoleScores)
     return (
         <div className='GolferScoreCard'>
             <h1>{props.player.name}</h1>
             <div className='playerScores'>
                 <div className='scoreCardHoles'>
-                    {props.holes.filter(hole => hole.course_id === props.currentCourse.id).map(hole => <h6>{hole.number}</h6>)}
+                    <h4>Hole #</h4>
+                    {playerHoles.map(hole => <h6 key={hole.id}>{hole.number}</h6>)}
                 </div>
                 <div className='scoreCardPars'>
-                    {props.holes.filter(hole => hole.course_id === props.currentCourse.id).map(hole => <h6>{hole.par}</h6>)}
+                    <h4>Par</h4>
+                    {playerHoles.map(hole => <h6 key={hole.id}>{hole.par}</h6>)}
+                </div>
+                <div className='scoreCardShots'>
+                    <h4>Shots</h4>
+                    {playerHoleScores.map(phs=> <h6 key={phs.id}>{phs.shots}</h6>)}
                 </div>
                 <div className='scoreCardScores'>
-                    {props.holeScores.filter(holeScore => holeScore.course_score_id === props.currentCourseScores.find(courseScore => courseScore.player_id === props.player.id).id)
-                        .map(chs=> <h6>{chs.score}</h6>)}
-                </div>
-                <div className='scoreCardResults'>
+                    <h4>Score</h4>
+                    {playerHoleScores.map(phs => <h6 key={phs.id}>{phs.shots - playerHoles.find(hole => hole.id === phs.hole_id).par}</h6>)}
                 </div>
             </div>
         </div>
--- src/store.js
+++ src/store.js
@@ -4,14 +4,35 @@ import uuid from 'uuid/v4'
 // set up default state
 const defaultState = {
     courses: [{id: 1, name: 'Golf Course'}], // all the courses
-    holes: [], // all the holes
+    holes: [
+        {id: 3, course_id: 1, number: 1, par: 3},
+        {id: 4, course_id: 1, number: 2, par: 4},
+        {id: 5, course_id: 1, number: 3, par: 5},
+        {id: 6, course_id: 1, number: 4, par: 4},
+        {id: 7, course_id: 1, number: 5, par: 5},
+        {id: 8, course_id: 1, number: 6, par: 3},
+        {id: 9, course_id: 1, number: 7, par: 4},
+        {id: 10, course_id: 1, number: 8, par: 3},
+        {id: 11, course_id: 1, number: 9, par: 5},
+        {id: 12, course_id: 1, number: 10, par: 4},
+        {id: 13, course_id: 1, number: 11, par: 3},
+        {id: 14, course_id: 1, number: 12, par: 5},
+        {id: 15, course_id: 1, number: 13, par: 4},
+        {id: 16, course_id: 1, number: 14, par: 4},
+        {id: 17, course_id: 1, number: 15, par: 3},
+        {id: 18, course_id: 1, number: 16, par: 3},
+        {id: 19, course_id: 1, number: 17, par: 5},
+        {id: 20, course_id: 1, number: 18, par: 5},
+    ], // all the holes
     players: [{id: 2, name: 'Player 1'}], // all the players
-    courseScores: [], // all the scores
-    holeScores: [], // all the hole scores
+    courseScores: [{id: 21, course_id: 1, player_id: 2}], // all the scores
+    holeScores: [
+        {id: 22, course_score_id: 21, hole_id: 3, shots: 3},
+    ], // all the hole scores
     currentCourse: {id: 1, name: 'Golf Course'}, // the current course
     // currentHole: {},
     currentPlayers: [{id: 2, name: 'Player 1'}], // the current players
-    currentCourseScores: [], // the current scores
+    currentCourseScores: [{id: 21, course_id: 1, player_id: 2}], // the current scores
     // currentHoleScores: [], 
     // searchTerm: ''
 }
@@ -110,14 +131,14 @@ export const addCourseScore = ({player_id, course_id}) => {
         }
     }
 }
-export const addHoleScore = ({course_score_id, hole_id, score}) => {
+export const addHoleScore = ({course_score_id, hole_id, shots}) => {
     return {
         ...ADD_HOLE_SCORE,
         newHoleScore: {
             id: uuid(),
             course_score_id, 
             hole_id,
-            score
+            shots
         }
     }
 }
@@ -161,14 +182,14 @@ export const updateCourseScore = ({id, course_id, player_id}) => {
         }
     }
 }
-export const updateHoleScore = ({id, course_score_id, hole_id, score}) => {
+export const updateHoleScore = ({id, course_score_id, hole_id, shots}) => {
     return {
         ...UPDATE_HOLE_SCORE,
         updatedHoleScore: {
             id,
             course_score_id,
             hole_id,
-            score
+            shots
         }
     }
 }
@@ -218,14 +239,14 @@ export const deleteCourseScore = ({id, course_id, player_id}) => {
         }
     }
 }
-export const deleteHoleScore = ({id, course_score_id, hole_id, score}) => {
+export const deleteHoleScore = ({id, course_score_id, hole_id, shots}) => {
     return {
         ...DELETE_HOLE_SCORE,
         holeScoreToDelete: {
             id,
             course_score_id,
             hole_id,
-            score
+            shots
         }
     }
 }
