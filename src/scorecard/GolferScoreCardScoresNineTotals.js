dc83d05bef6ea7703da42335074900fdf6d51a35
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-19 18:27:54 -0500

    add reset button function

--- src/scorecard/GolferScoreCardScoresNineTotals.js
+++ src/scorecard/GolferScoreCardScoresNineTotals.js
@@ -7,11 +7,12 @@ export default function GolferScoreCardScoresNineTotals(props) {
         <h6>{totalsRowLabel}</h6>
         <h6>{playerHoles.slice(startHoleNum, endHoleNum)
             .reduce((prevHole, currHole) => {
-            return prevHole.par ? prevHole.par + currHole.par : prevHole + currHole.par
-        })}</h6>
+                return prevHole.par ? prevHole.par + currHole.par : prevHole + currHole.par
+            })}</h6>
         <h6>{playerHoleScores.slice(startHoleNum, endHoleNum)
             .reduce((prevHole, currHole) => {
-            return prevHole.shots ? prevHole.shots + currHole.shots : prevHole + currHole.shots
+            console.log(prevHole, currHole)
+            return prevHole.shots || prevHole.shots === 0 ? prevHole.shots + currHole.shots : prevHole + currHole.shots
         })}</h6>
         <h6>{playerHoleScores.slice(startHoleNum, endHoleNum)
             .map((holeScore, index) => holeScore.shots - playerHoles[index].par)
--- src/scorecard/GolferScoreCardTotals.js
+++ src/scorecard/GolferScoreCardTotals.js
@@ -9,7 +9,7 @@ export default function GolferScoreCardTotals(props) {
                 return prevHole.par ? prevHole.par + currHole.par : prevHole + currHole.par
             })}</h6>
             <h6>{playerHoleScores.reduce((prevHole, currHole) => {
-                return prevHole.shots ? prevHole.shots + currHole.shots : prevHole + currHole.shots
+                return prevHole.shots || prevHole.shots === 0 ? prevHole.shots + currHole.shots : prevHole + currHole.shots
             })}</h6>
             <h6>{playerHoleScores.map((holeScore, index) => holeScore.shots - playerHoles[index].par)
             .reduce((prevScore, currScore) => prevScore + currScore)}</h6>
