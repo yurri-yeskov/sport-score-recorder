8b48e468f69e6ea9c0a62d19dd36c4cfeeb84260
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-12-20 14:37:52 -0500

    fix issue where multiple over holes shows even for nine

--- src/scorecard/GolferScoreCardScoresNineTotals.js
+++ src/scorecard/GolferScoreCardScoresNineTotals.js
@@ -6,10 +6,20 @@ export default function GolferScoreCardScoresNineTotals(props) {
         .reduce((prevHole, currHole) => {
             return prevHole.shots || prevHole.shots === 0 ? prevHole.shots + currHole.shots : prevHole + currHole.shots
         })
-    const nineTotalScore = playerHoleScores.slice(startHoleNum, endHoleNum)
-    .filter(score => score.shots > 0)
-    .map((holeScore, index) => holeScore.shots - playerHoles[index].par) || 0
-    .reduce((prevScore, currScore) => prevScore + currScore)
+    let nineTotalScore = playerHoleScores.slice(startHoleNum, endHoleNum)
+    .filter(score => {
+        console.log('filter')
+        return score.shots > 0
+    })
+    .map((holeScore, index) => {
+        console.log('map')
+        return holeScore.shots - playerHoles[index].par
+    })
+    nineTotalScore = nineTotalScore.length > 0 ? nineTotalScore.reduce((prevScore, currScore) => {
+                                                    console.log('reduce')
+                                                    return prevScore + currScore
+                                                }) : 0
+    
     return (
     <div className='GolferScoreCardScoresNineTotals'>
         <h6>
